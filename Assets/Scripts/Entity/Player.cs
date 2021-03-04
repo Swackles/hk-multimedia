0b0fbf7f5344ab684d4225569aa590fe7e55dcbb
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-03-04 22:51:02 +0200

    Added jumping

--- Assets/Scripts/Entity/Player.cs
+++ Assets/Scripts/Entity/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 namespace Assets.Scripts.Entity
 {
@@ -10,15 +11,61 @@ namespace Assets.Scripts.Entity
 
         public int collectible;
         public Text counter;
+        [SerializeField] private LayerMask m_WhatIsGround;
+	    [SerializeField] private Transform m_GroundCheck;
+        [SerializeField] private float m_JumpForce = 400f;
+        const float k_GroundedRadius = .2f; 
+	    private bool m_Grounded;
+        public UnityEvent OnLandEvent;
+        [System.Serializable]
+	    public class BoolEvent : UnityEvent<bool> { }
+        bool jump = false;
+        
+
+        private void Awake()
+        {
+
+            if (OnLandEvent == null)
+                OnLandEvent = new UnityEvent();
+        }
 
         new public void Start() {
             base.Start();
             changeCounter();
 
         }
+
+        new public void FixedUpdate() {
+            base.FixedUpdate();
+            bool wasGrounded = m_Grounded;
+		    m_Grounded = false;
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject != gameObject)
+                {
+                    m_Grounded = true;
+                    if (!wasGrounded)
+                        Debug.Log("Hello!");
+                        OnLandEvent.Invoke();
+                }
+            }
+        }
+
+        public void OnLanding () {
+            _Animator.SetBool("Jumping", false);
+            Debug.Log("Hello!");
+        }
+
         public void Update()
         {
             Movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+            if (Input.GetButtonDown("Jump")) {
+                jump = true;
+                _Animator.SetBool("Jumping", true);
+                rb.AddForce(new Vector2(0f, m_JumpForce));
+            }
         }
 
         void OnTriggerEnter2D(Collider2D other) 
@@ -35,6 +82,7 @@ namespace Assets.Scripts.Entity
         public void changeCounter() {
             counter.text = "Collectibles: " + collectible;
         }
+
     }
 }
 
