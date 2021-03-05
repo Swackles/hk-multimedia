45e28708e7e531b4c196b2ca7d5b3ced658da250
Swackles <Swackles@users.noreply.github.com>
2021-03-05 10:08:57 +0200

    Added back collecting logic

--- Assets/Scripts/Entity/Player.cs
+++ Assets/Scripts/Entity/Player.cs
@@ -9,6 +9,12 @@ namespace Assets.Scripts.Entity
         public int collectible;
         public Text counter;
         public float JumpVelocity = 100f;
+        new public void Start()
+        {
+            base.Start();
+            changeCounter();
+
+        }
 
         public void Update()
         {
@@ -16,6 +22,17 @@ namespace Assets.Scripts.Entity
 
             if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
                 rb.velocity = Vector2.up * JumpVelocity;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+
+            if (other.gameObject.CompareTag("Collectible"))
+            {
+                other.gameObject.SetActive(false);
+                collectible = collectible + 1;
+                changeCounter();
+            }
         }
 
         public void changeCounter() {
