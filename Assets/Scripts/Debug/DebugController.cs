eb4d4722e344405f5818b555b8a5a9034b311fb2
Swackles <Swackles@users.noreply.github.com>
2021-03-07 17:41:08 +0200

    #16 added debug menu

---
+++ Assets/Scripts/Debug/DebugController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Debug
+{
+    [DisallowMultipleComponent]
+    class DebugController : MonoBehaviour
+    {
+        [SerializeField] private bool ActivateOnStartup = true;
+        [SerializeField] private KeyCode Key = KeyCode.F2;
+        private GameObject DebugGrid;
+
+        public void Start()
+        {
+            DebugGrid = GetComponentInChildren<GridLayoutGroup>().gameObject;
+
+            DebugGrid.SetActive(ActivateOnStartup);
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(Key))
+                DebugGrid.SetActive(!DebugGrid.activeSelf);
+        }
+    }
+}
---
+++ Assets/Scripts/Debug/FPSCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Debug
+{
+    [RequireComponent(typeof(Text))]
+    class FPSCounter : MonoBehaviour
+    {
+        private Text _Text;
+        public void Awake()
+        {
+            _Text = GetComponent<Text>();
+        }
+        public void Update()
+        {
+            
+            float frames = Time.frameCount / Time.time;
+
+            _Text.text = "FPS: " + ((int)frames).ToString();
+        }
+    }
+}
