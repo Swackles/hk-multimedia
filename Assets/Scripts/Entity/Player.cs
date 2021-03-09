471e1accb6d03d444ec76199128fd899f27d1ee6
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-03-09 19:05:46 +0200

    Life lost
    Added a life counter, some example box enemies and knockback from enemies.

--- Assets/Scripts/Entity/Player.cs
+++ Assets/Scripts/Entity/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +10,9 @@ namespace Assets.Scripts.Entity
 
         public int collectible;
         public Text counter;
+        public float enemyBumpPowerY = 10f;
+        public float enemyBumpPowerX = 10f;
+        public int health = 3;
 
         new public void Start() {
             base.Start();
@@ -35,6 +38,27 @@ namespace Assets.Scripts.Entity
         public void changeCounter() {
             counter.text = "Collectibles: " + collectible;
         }
+
+        void OnCollisionEnter2D(Collision2D collisionEnemy)
+            {
+            if (collisionEnemy.gameObject.tag.Equals ("Enemy"))
+                {
+                    if (sr.flipX) 
+                        {
+                            rb.velocity = Vector2.right * enemyBumpPowerX;
+                        } else {
+                            rb.velocity = Vector2.left * enemyBumpPowerX;
+                        }
+                    if (health > 1) 
+                        {
+                            health = health - 1;
+                    } else
+                        {
+                            rb.transform.position = new Vector2(Input.GetAxis("Horizontal"), 0);
+                            health = 3;
+                        }
+                }
+            }
     }
 }
 
