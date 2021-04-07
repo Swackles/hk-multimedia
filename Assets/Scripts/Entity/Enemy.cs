267d6f85cc4c079a6d240a589ef11ca4513dc85a
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-04-08 00:57:30 +0300

    Life-lost added
    New branch from master.
    Added life-lost/hurt.

---
+++ Assets/Scripts/Entity/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Entity
+{
+    class Enemy : MonoBehaviour
+    {
+        public float HitPower = 100f;
+
+        void OnCollisionEnter2D(Collision2D collidedWith) {
+            if (collidedWith.gameObject.CompareTag("Player")) {
+                Vector2 KnockBack = collidedWith.gameObject.transform.position - transform.position;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().hurt(KnockBack * HitPower);
+            }
+        }
+    }
+}
