8ddb4e06222eb00ba29a99972f522dee118c6c11
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-04-27 22:12:11 +0300

    Made changed according to review
    Made changes according to review.

--- Assets/Scripts/Entity/Granny.cs
+++ Assets/Scripts/Entity/Granny.cs
@@ -1,74 +1,53 @@
-﻿//Patrolpoints from a prefab must be assigned to Grannys array before patrolling can work.
+﻿//_patrolPoints from a prefab must be assigned to Grannys array before patrolling can work.
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
 namespace Assets.Scripts.Entity
 {
     public class Granny : AbstractEntity
     {
-        private float guardTime;
-        //Total guarding time set to 3 by default for testing.
-        public float totalGuardTime = 3;
-        public Transform[] patrolPoints;
-        private int randomPoint;
-        private int oldRandomPoint;
-        public Vector2 newPoint;
-        public Vector2 oldPoint;
-
+        [SerializeField] private Transform[] _patrolPoints;
+        [SerializeField] private double _guardFor;
+        private int _randomPoint;
+        private static System.Timers.Timer _guardTimer;
+        private int _guardTime = 1;
+        
         //Granny selects a random patrol point from the array. Consecutive selections of the same point are avoided.
-        new public void Start()
+        new private void Start()
         {
-            //Speed set to 10f by default for testing.
-            Speed = 10f;
             base.Start();
-            guardTime = totalGuardTime;
-            randomPoint = Random.Range(0, patrolPoints.Length);
-            Debug.Log("Hello!");
-            if(randomPoint == oldRandomPoint){
-                randomPoint = Random.Range(0, patrolPoints.Length);
-            }
-            oldRandomPoint = randomPoint;
-
-            oldPoint = transform.position;
+            _guardTimer = new System.Timers.Timer(_guardFor);
+            _guardTimer.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => _guardTime = 0;
+            _randomPoint = Random.Range(0, _patrolPoints.Length);
         }
         //Overwrite base of abstractentity.
         new private void FixedUpdate(){ }
 
         //Animation, flip, timing and movement from point to point.
-        public void Update()
-        {
-            newPoint = patrolPoints[randomPoint].position;
-            if(newPoint.x > oldPoint.x){
-                SR.flipX = false;
-            }
-            if(newPoint.x < oldPoint.x){
-                SR.flipX = true;
-            }
+        private void Update()
+        {   
+            transform.position = Vector2.MoveTowards(transform.position, _patrolPoints[_randomPoint].position, Speed * Time.deltaTime);
 
-            Animator.SetBool("Patrolling", true);
-            Animator.SetBool("Idle", false);
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[randomPoint].position, Speed * Time.deltaTime);
-
-
-            if(Vector2.Distance(transform.position, patrolPoints[randomPoint].position) < 0.2f){
-                if(guardTime <= 0){
-                    randomPoint = Random.Range(0, patrolPoints.Length);
-                    while(randomPoint == oldRandomPoint){
-                        randomPoint = Random.Range(0, patrolPoints.Length);
+            if(Vector2.Distance(transform.position, _patrolPoints[_randomPoint].position) < 0.2f){
+                if(_guardTime <= 0){
+                    _guardTimer.Interval = _guardFor;
+                    _randomPoint = Random.Range(0, _patrolPoints.Length);
+                    while(_patrolPoints[_randomPoint].position.x == transform.position.x){
+                        _randomPoint = Random.Range(0, _patrolPoints.Length);
                     }
-                    oldRandomPoint = randomPoint;
-                    guardTime = totalGuardTime;
+                    _guardTime = 1;
+                    SR.flipX = _patrolPoints[_randomPoint].position.x < transform.position.x;
+                    Animator.SetBool("Patrolling", true);
+                    Animator.SetBool("Idle", false);
                 } else {
                     Animator.SetBool("Idle", true);
                     Animator.SetBool("Patrolling", false);
-                    guardTime -= Time.deltaTime;
-                    oldPoint = transform.position;
+                    _guardTimer.Start();
                 }
             }
         }
-
-        
     }
 }
