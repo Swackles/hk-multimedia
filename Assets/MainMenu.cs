d6c7e3f3e0b48e5cf0213f1e4ae68d5c98b00181
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-05-10 18:40:07 +0300

    Added main menu
    Added main menu:
    -Currently font is default
    -No content on How to play and Credits at the moment
    -Play works just fine
    -Volume slider works, but might need tuning, currently set to max,min values of unity audiomixer

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log message is for testing, since Application.Quit doesnt close the game in editor.
+        Debug.Log ("Game Closed");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu/VolumeControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolumeTo(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
