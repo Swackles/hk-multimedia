0b054132d51fb92caf49438af5d30fcba6ed3c37
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-05-10 22:14:24 +0300

    Changed according to review
    Changes:
    -Added sound to menu
    -Added namespaces
    -Moved camera
    -Removed old Main menu script(duplicate)
    -Removed unused references
    -Removed debug message
    
    No faults found with volume control carrying over to the other scene.

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace Assets.Scripts.Menu
 {
-    public void StartGame()
+    public class MainMenu : MonoBehaviour
     {
-        SceneManager.LoadScene(1);
-    }
+        public void StartGame()
+        {
+            SceneManager.LoadScene(1);
+        }
 
-    public void QuitGame()
-    {
-        Debug.Log ("Quit!");
-        Application.Quit();
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
     }
 }
--- Assets/Scripts/Menu/VolumeControl.cs
+++ Assets/Scripts/Menu/VolumeControl.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 
-public class VolumeControl : MonoBehaviour
+namespace Assets.Scripts.Menu
 {
-    public AudioMixer audioMixer;
-
-    public void SetVolumeTo(float volume)
+    public class VolumeControl : MonoBehaviour
     {
-        audioMixer.SetFloat("Volume", volume);
+        public AudioMixer audioMixer;
+
+        public void SetVolumeTo(float volume)
+        {
+            audioMixer.SetFloat("Volume", volume);
+        }
     }
 }
