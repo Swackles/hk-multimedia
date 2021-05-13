6d4c88018e63eca04c93bbb303912328e1077eb3
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-05-13 22:03:35 +0300

    Added main menu (#64)
    * Added main menu
    
    Added main menu:
    -Currently font is default
    -No content on How to play and Credits at the moment
    -Play works just fine
    -Volume slider works, but might need tuning, currently set to max,min values of unity audiomixer
    
    * Changed according to review
    
    Changes:
    -Added sound to menu
    -Added namespaces
    -Moved camera
    -Removed old Main menu script(duplicate)
    -Removed unused references
    -Removed debug message
    
    No faults found with volume control carrying over to the other scene.
    
    * Changes according to review and meeting
    
    Changes:
    -Replaced volume slider with mute button
    -Updated button sprites
    -Added background to main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+namespace Assets.Scripts.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        private bool _mute;
+
+        public void SoundToggle()
+        {
+            _mute = !_mute;
+            AudioListener.pause = _mute;
+        }
+    }
+}
