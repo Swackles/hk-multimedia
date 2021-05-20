4014c3acfcc901fc38e80d26b82d7d5fb10f05d2
Swackles <Swackles@users.noreply.github.com>
2021-05-20 18:56:07 +0300

    added sound effects to virus

---
+++ Assets/Scripts/AudioPlayer/VirusAudioPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.AudioPlayer
+{
+    [Serializable]
+    public class VirusAudioPlayer : AbstractAudioPlayer
+    {
+        public AudioClip Attack;
+        public AudioClip AttackCooldownReset;
+    }
+}
