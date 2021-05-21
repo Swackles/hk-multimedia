86e8a91319f730514787d0ed12954386e5b4eae7
Swackles <Swackles@users.noreply.github.com>
2021-05-21 14:16:57 +0300

    Grandma now obides by the laws of physics

--- Assets/Scripts/Entity/Granny.cs
+++ Assets/Scripts/Entity/Granny.cs
@@ -8,42 +8,68 @@ namespace Assets.Scripts.Entity
     public class Granny : Enemy
     {
         [SerializeField] private Transform[] _patrolPoints;
+        [Tooltip("The amount of time grandma will guard for")]
         [SerializeField] private double _guardFor;
+
         private int _randomPoint;
-        private static Timer _guardTimer;
-        private int _guardTime = 1;
+        private bool _guarding = false;
+        private Timer _guardingTimer = new Timer();
+
+        // Returns the current patrol point
+        public Vector2 PatrolPoint {
+            get { return new Vector2(_patrolPoints[_randomPoint].position.x, transform.position.y); }
+        }
         
         //Granny selects a random patrol point from the array. Consecutive selections of the same point are avoided.
         new private void Start()
         {
             base.Start();
-            _guardTimer = new Timer(_guardFor);
-            _guardTimer.Elapsed += (object sender, ElapsedEventArgs e) => _guardTime = 0;
-            _randomPoint = Random.Range(0, _patrolPoints.Length);
+
+            SetPatrolPoint();
         }
 
         //Animation, flip, timing and movement from point to point.
-        private void Update()
-        {   
-            transform.position = Vector2.MoveTowards(transform.position, _patrolPoints[_randomPoint].position, Speed * Time.deltaTime);
+        private void FixedUpdate()
+        {
+            if (_guarding && !_guardingTimer.Enabled)
+                EndGuard();
+            else if (!_guarding)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, PatrolPoint, Speed * Time.deltaTime);
 
-            if(Vector2.Distance(transform.position, _patrolPoints[_randomPoint].position) < 0.2f){
-                if(_guardTime <= 0){
-                    _guardTimer.Interval = _guardFor;
-                    _randomPoint = Random.Range(0, _patrolPoints.Length);
-                    while(_patrolPoints[_randomPoint].position.x == transform.position.x){
-                        _randomPoint = Random.Range(0, _patrolPoints.Length);
-                    }
-                    _guardTime = 1;
-                    SR.flipX = _patrolPoints[_randomPoint].position.x < transform.position.x;
-                    Animator.SetBool("Patrolling", true);
-                    Animator.SetBool("Idle", false);
-                } else {
-                    Animator.SetBool("Idle", true);
-                    Animator.SetBool("Patrolling", false);
-                    _guardTimer.Start();
-                }
+                if (Vector2.Distance(transform.position, PatrolPoint) < 0.2f)
+                    StartGuard();
             }
+        }
+
+        public void StartGuard()
+        {
+            Animator.SetBool("Idle", true);
+            Animator.SetBool("Patrolling", false);
+
+            _guardingTimer = new Timer(_guardFor);
+            _guardingTimer.AutoReset = false;
+            _guardingTimer.Start();
+
+            _guarding = true;
+        }
+
+        public void EndGuard()
+        {
+            Animator.SetBool("Patrolling", true);
+            Animator.SetBool("Idle", false);
+
+            SetPatrolPoint();
+
+            SR.flipX = PatrolPoint.x < transform.position.x;
+
+            _guarding = false;
+        }
+
+        private void SetPatrolPoint()
+        {
+            while (Vector2.Distance(transform.position, PatrolPoint) < 0.2f)
+                _randomPoint = Random.Range(0, _patrolPoints.Length);
         }
     }
 }
