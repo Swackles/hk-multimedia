dd70d1fc39fd4001db0a76a482aecfd8ce5e88f4
Swackles <Swackles@users.noreply.github.com>
2021-06-09 18:27:11 +0300

    fixed knockback issue

--- Assets/Scripts/Entity/Enemy.cs
+++ Assets/Scripts/Entity/Enemy.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts.Entity
         public void OnCollisionEnter2D(Collision2D collidedWith) {
             if (collidedWith.gameObject.CompareTag("Player")) {
                 Vector2 KnockBack = collidedWith.gameObject.transform.position - transform.position;
-                GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().Hurt(KnockBack * HitPower);
+                GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().Hurt(KnockBack.normalized * HitPower);
             }
         }
     }
