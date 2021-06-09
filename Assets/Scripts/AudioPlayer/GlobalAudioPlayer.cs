0ece88363419dec3fcf058a799f3f0e1706efe31
Swackles <Swackles@users.noreply.github.com>
2021-06-09 17:51:46 +0300

    added background music, game win and lose effect

--- Assets/Scripts/AudioPlayer/GlobalAudioPlayer.cs
+++ Assets/Scripts/AudioPlayer/GlobalAudioPlayer.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
+using Assets.Scripts.EventSystems;
 
 namespace Assets.Scripts.AudioPlayer
 {
     [RequireComponent(typeof(AudioSource))]
-    class GlobalAudioPlayer : MonoBehaviour
+    class GlobalAudioPlayer : MonoBehaviour, IGameFinished, IPlayerDeathHandler
     {
         public static GlobalAudioPlayer Current;
-        private AudioSource Source;
+        [SerializeField] private AudioSource _source, _backgroundMusic;
+        [SerializeField] private AudioClip _gameOver, _gameWin;
 
-        public void Awake()
-        {
-            Source = GetComponent<AudioSource>();
-            
-            Current = this;
-        }
+        public void Awake() { Current = this; }
 
         /// <summary>
         /// Plays the specified audio clip in global audio player
@@ -21,8 +18,20 @@ namespace Assets.Scripts.AudioPlayer
         /// <param name="clip">clip to be played</param>
         public void Play(AudioClip clip)
         {
-            Source.clip = clip;
-            Source.Play();
+            _source.clip = clip;
+            _source.Play();
+        }
+
+        public void OnGameFinished()
+        {
+            _backgroundMusic.Pause();
+            Play(_gameWin);
+        }
+
+        public void OnPlayerDeath(Entity.Player player)
+        {
+            _backgroundMusic.Pause();
+            Play(_gameOver);
         }
     }
 }
